d4f9faf7b24f64d199d73907bb6ebdf56da41740
omar <paysitemas@gmail.com>
2020-12-09 20:26:49 -0600

    primero

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { DetalleComponent } from './components/detalle/detalle.component';
+import { UpdateProductComponent } from './components/update-product/update-product.component';
+import { CarrouselComponent } from './components/carrousel/carrousel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'carrousel', component: CarrouselComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: 'detalle', component: DetalleComponent},
+  {path: 'updateProduct', component: UpdateProductComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'carrousel'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetalleComponent } from './components/detalle/detalle.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { UpdateProductComponent } from './components/update-product/update-product.component';
+import { FooterComponent } from './components/structure/footer/footer.component';
+import { HeadComponent } from './components/structure/head/head.component';
+import { CarrouselComponent } from './components/carrousel/carrousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetalleComponent,
+    ProductListComponent,
+    UpdateProductComponent,
+    FooterComponent,
+    HeadComponent,
+    CarrouselComponent
   ],
   imports: [
     BrowserModule,
